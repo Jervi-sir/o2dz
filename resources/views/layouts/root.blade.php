a7b9486321d42f74c5e4171dbf1cc4c4191a5e4c
Jervi <gacembekhira@gmail.com>
2021-08-06 17:33:33 +0100

    took off some meta tags

--- resources/views/layouts/root.blade.php
+++ resources/views/layouts/root.blade.php
@@ -7,20 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="oxygene, algerie, dz, o2dz, fournisseur d'oxygene, help, site oxygene">
     <meta name="author" content="2kings-websites Gacem aka Jervi, Aymen aka iverj">
-    <meta name="publisher" content="2kings-websites">
     <meta name="copyright" content="2kings-websites">
     <meta name="description" content="موقع اين يمكنك العثور على معدات الأكسجين ، كما يمكنك الإعلان عن عروضك إذا كان لديك معدات أكسجين تريد توفيرها">
-    <meta name="page-topic" content="Media">
-    <meta name="audience" content="Everyone">
     <link rel="stylesheet" href="css/main.css">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/png" href="images/logo.svg"/>
     <link rel="stylesheet" href="css/toastr.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script src="js/toastr.js"></script>
-    <noscript>
-        This page needs JavaScript activated to work.
-     </noscript>
+    
     @yield('title')
 </head>
 <body>
