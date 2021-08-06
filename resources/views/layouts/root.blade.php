09a6aa9c5b577f6d871d2a7024bdb418a2460972
Jervi <gacembekhira@gmail.com>
2021-08-06 17:38:47 +0100

    updated meta added og meta

--- resources/views/layouts/root.blade.php
+++ resources/views/layouts/root.blade.php
@@ -2,13 +2,22 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge, chrome=1">
     <meta name="Content-Language" content="arabic">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <link rel="canonical" href="https://o2dz.com/">
     <meta name="keywords" content="oxygene, algerie, dz, o2dz, fournisseur d'oxygene, help, site oxygene">
     <meta name="author" content="2kings-websites Gacem aka Jervi, Aymen aka iverj">
     <meta name="copyright" content="2kings-websites">
     <meta name="description" content="موقع اين يمكنك العثور على معدات الأكسجين ، كما يمكنك الإعلان عن عروضك إذا كان لديك معدات أكسجين تريد توفيرها">
+    
+    
+    <meta property="og:type" content="website">
+    <meta property="og:title" content=" O2dz - trouver des equipements d'oxygene ">
+    <meta property="ogdescription" content="موقع اين يمكنك العثور على معدات الأكسجين ، كما يمكنك الإعلان عن عروضك إذا كان لديك معدات أكسجين تريد توفيرها">
+    <meta property="og:link" href="https://o2dz.com/">
+    <meta property="og:site_name" href="o2dz">
+
     <link rel="stylesheet" href="css/main.css">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/png" href="images/logo.svg"/>
